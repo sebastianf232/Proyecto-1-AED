e93cbf665d13f30e814547c8679443d30bfb873e
sebastianf232 <87022337+sebastianf232@users.noreply.github.com>
2022-03-08 11:22:37 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Proyecto 1
+ * @author Juan Miguel Gonzalez-Campo 21077
+ * @author Sebastian Franco 21484
+ * @author Gabriela de Leon 21037
+ * Clase Main
+ */
+
 public class Main{
     static converter conv = new converter();
 

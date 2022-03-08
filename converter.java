e6bc687223e20eb7234a209f6616015705b86344
sebastianf232 <87022337+sebastianf232@users.noreply.github.com>
2022-03-08 11:23:49 -0600

    Update converter.java

--- converter.java
+++ converter.java
@@ -1,8 +1,18 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Proyecto 1
+ * @author Juan Miguel Gonzalez-Campo 21077
+ * @author Sebastian Franco 21484
+ * @author Gabriela de Leon 21037
+ * Clase Converter
+ */
+
 public class converter {
 
     public boolean isOperator(char x)
     {
-        switch (x) {
+        switch (x) { ///Se evalua cada operador para ver si existe
         case '+':
         case '-':
         case '/':

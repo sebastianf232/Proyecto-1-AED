1d804cb4455f5e1f9cd0d1ac36db8ec107b0c896
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2022-03-15 11:44:57 -0600

    Prueba stack

---
+++ StackTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackTest {
+    private final Stack<Double> s = new Stack<>();
+    //double sc;
+    @Test
+    void add() {
+        s.add(5.0);
+        s.add(4.0);
+    }
+
+    @Test
+    void remove() {
+        s.add(5.0);
+        s.add(4.0);
+        assertEquals(4,s.remove(),"Hay un error al remover el elemento del stack");
+    }
+
+    @Test
+    void peek(){
+        s.add(5.0);
+        s.add(4.0);
+        assertEquals(4,s.peek(),"Hay un error al obtener el objeto del stack");
+    }
+
+    @Test
+    void size(){
+        s.add(5.0);
+        s.add(4.0);
+        assertEquals(2,s.size(),"Hay un error al obtener el tamaño del objeto del stack");
+    }
+
+    @Test
+    void empty(){
+
+    }
+}

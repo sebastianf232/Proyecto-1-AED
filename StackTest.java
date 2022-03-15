d2a74231c749d3fe13fa3f52ddad9608f4d4a56b
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2022-03-15 11:46:48 -0600

    Update StackTest.java

--- StackTest.java
+++ StackTest.java
@@ -1,3 +1,12 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Proyecto 1
+ * @author Juan Miguel Gonzalez-Campo 21077
+ * @author Sebastian Franco 21484
+ * @author Gabriela de León 21037
+ * Prueba unitaria Stack
+ */
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;

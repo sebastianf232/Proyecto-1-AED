5cbcba0f65a3a120b3a25f314320e69b51001cab
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2022-03-22 15:23:29 -0600

    Calculator Test

---
+++ calculatorTest.java
@@ -0,0 +1,33 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Proyecto 1
+ * @author Juan Miguel Gonzalez-Campo 21077
+ * @author Sebastian Franco 21484
+ * @author Gabriela de León 21037
+ * Prueba unitaria calculator
+ */
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class calculatorTest {
+    String c;
+    ArrayList<String> a = new ArrayList<>();
+    @Test
+    void isOperand(){
+    }
+    
+    @Test
+    void evaluatePrefix(){
+    }
+    
+    @Test
+    void calculate(){
+    }
+    
+    @Test
+    void ifZero(){
+    }
+}
